5b0733f1b9054954cf8352cf7dd9fea0e0b5669a
Asier <29598090+AsierDev@users.noreply.github.com>
2020-04-19 13:31:07 +0200

    Add app component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('app'));
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
